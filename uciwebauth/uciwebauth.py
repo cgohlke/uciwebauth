3dbc23611e06bf965973b84f38fcca995e9b48f7
Christoph Gohlke <cgohlke@uci.edu>
2018-10-19 14:19:01 -0700

    Update uciwebauth/uciwebauth.py

--- uciwebauth/uciwebauth.py
+++ uciwebauth/uciwebauth.py
@@ -9,19 +9,21 @@
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
-# * Redistributions of source code must retain the above copyright
-#   notice, this list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above copyright
-#   notice, this list of conditions and the following disclaimer in the
-#   documentation and/or other materials provided with the distribution.
-# * Neither the name of the copyright holders nor the names of any
-#   contributors may be used to endorse or promote products derived
-#   from this software without specific prior written permission.
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
 # LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
 # CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
 # SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
@@ -32,7 +34,7 @@
 
 """Access UCI WebAuth, LDAP person records, and Active Directory user objects.
 
-Uciwebauth is a library to access identity management and authentication
+Uciwebauth is a Python library to access identity management and authentication
 services at the University of California, Irvine (UCI):
 
 1. WebAuth provides a secure, single sign-on authentication solution tool
@@ -48,7 +50,7 @@ services at the University of California, Irvine (UCI):
 :Organization:
   Laboratory for Fluorescence Dynamics. University of California, Irvine
 
-:Version: 2018.9.28
+:Version: 2018.10.18
 
 Requirements
 ------------
@@ -80,9 +82,7 @@ References
 
 """
 
-from __future__ import division, print_function
-
-__version__ = '2018.9.28'
+__version__ = '2018.10.18'
 __docformat__ = 'restructuredtext en'
 __all__ = ('WebAuth', 'WebAuthError', 'LdapPerson', 'LdapPersonError',
            'AdsiUser', 'AdsiUserError', 'WebAuthBackend')
