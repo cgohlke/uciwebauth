23618515a85f0408694cca00cdc9e184932016e2
Christoph Gohlke <cgohlke@uci.edu>
2019-12-31 22:13:14 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # uciwebauth/setup.py
 
 """Uciwebauth package setuptools script."""
@@ -12,18 +11,23 @@ with open('uciwebauth/uciwebauth.py') as fh:
     code = fh.read()
 
 version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
-description = re.search(r'"""(.*)\.[\r\n?|\n]', code).groups()[0]
-readme = re.search(r'[\r\n?|\n]{2}"""(.*)"""[\r\n?|\n]{2}__version__', code,
-                   re.MULTILINE | re.DOTALL).groups()[0]
-license = re.search(r'(# Copyright.*?[\r\n?|\n])[\r\n?|\n]+""', code,
+
+description = re.search(r'"""(.*)\.(?:\r\n|\r|\n)', code).groups()[0]
+
+readme = re.search(r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}__version__',
+                   code, re.MULTILINE | re.DOTALL).groups()[0]
+
+readme = '\n'.join([description, '=' * len(description)] +
+                   readme.splitlines()[1:])
+
+license = re.search(r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""', code,
                     re.MULTILINE | re.DOTALL).groups()[0]
 
-readme = '\n'.join([description, '=' * len(description)]
-                   + readme.splitlines()[1:])
 license = license.replace('# ', '').replace('#', '')
 
 if 'sdist' in sys.argv:
     with open('LICENSE', 'w') as fh:
+        fh.write('BSD 3-Clause License\n\n')
         fh.write(license)
     with open('README.rst', 'w') as fh:
         fh.write(readme)
@@ -37,12 +41,12 @@ setup(
     author_email='cgohlke@uci.edu',
     url='https://www.lfd.uci.edu/~gohlke/',
     packages=['uciwebauth'],
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     install_requires=[],
     extras_require={
         'ldap': ['python-ldap>=3.1'],
-        'adsi': ['pywin32>=223;platform_system=="Windows"'],
-        'all': ['python-ldap>=3.1', 'pywin32>=224;platform_system=="Windows"'],
+        'adsi': ['pywin32>=226;platform_system=="Windows"'],
+        'all': ['python-ldap>=3.1', 'pywin32>=226;platform_system=="Windows"'],
     },
     license='BSD',
     platforms=['any'],
@@ -52,8 +56,8 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        ],
+        'Programming Language :: Python :: 3.8',
+    ],
 )
