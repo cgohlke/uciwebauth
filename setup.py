f6797506a012c7159ff3e01294076a2538b04663
Christoph Gohlke <cgohlke@cgohlke.com>
2022-09-28 14:53:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,43 @@
 # uciwebauth/setup.py
 
-"""Uciwebauth package setuptools script."""
+"""Uciwebauth package Setuptools script."""
 
 import sys
 import re
 
 from setuptools import setup
 
+
+def search(pattern, code, flags=0):
+    # return first match for pattern in code
+    match = re.search(pattern, code, flags)
+    if match is None:
+        raise ValueError(f'{pattern!r} not found')
+    return match.groups()[0]
+
+
 with open('uciwebauth/uciwebauth.py') as fh:
     code = fh.read()
 
-version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
+version = search(r"__version__ = '(.*?)'", code)
 
-description = re.search(r'"""(.*)\.(?:\r\n|\r|\n)', code).groups()[0]
+description = search(r'"""(.*)\.(?:\r\n|\r|\n)', code)
 
-readme = re.search(
-    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}__version__',
+readme = search(
+    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}[__version__|from]',
     code,
     re.MULTILINE | re.DOTALL,
-).groups()[0]
+)
 
 readme = '\n'.join(
     [description, '=' * len(description)] + readme.splitlines()[1:]
 )
 
-license = re.search(
+license = search(
     r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""',
     code,
     re.MULTILINE | re.DOTALL,
-).groups()[0]
+)
 
 license = license.replace('# ', '').replace('#', '')
 
@@ -45,8 +54,8 @@ setup(
     description=description,
     long_description=readme,
     author='Christoph Gohlke',
-    author_email='cgohlke@uci.edu',
-    url='https://www.lfd.uci.edu/~gohlke/',
+    author_email='cgohlke@cgohlke.com',
+    url='https://www.cgohlke.com',
     project_urls={
         'Bug Tracker': 'https://github.com/cgohlke/uciwebauth/issues',
         'Source Code': 'https://github.com/cgohlke/uciwebauth',
@@ -79,5 +88,6 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 )
